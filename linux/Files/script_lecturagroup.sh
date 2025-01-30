cd10a106175c9516c94f4fdfb1bb4cbce33754f0
brantest174 <bran.test27@gmail.com>
2025-01-29 20:33:50 -0600

    Add: movimiento de archivos

--- linux/Files/script_lecturagroup.sh
+++ linux/Files/script_lecturagroup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 TEXT=$(ls test.txt)
 FILTERS=$(more filter.txt)
 
@@ -6,3 +7,4 @@ do
 	echo "${filter}:"
 	grep -c ${filter} $TEXT
 done
+#counting and grouping according to filters in a file
